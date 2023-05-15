7410d324b5da1d3ff02cf32e5ec8e2948cabeb9d
Santiago Salas <santiago.salas.contractor@bbva.com>
2023-05-15 12:36:54 -0500

    initial commit

--- src/main/java/com/fundaments/springboot/fundaments/FundamentsApplication.java
+++ src/main/java/com/fundaments/springboot/fundaments/FundamentsApplication.java
@@ -9,6 +9,7 @@ public class FundamentsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentsApplication.class, args);
 		System.out.println("Start Fundaments of Spring Boot Project");
+
 	}
 
 }
