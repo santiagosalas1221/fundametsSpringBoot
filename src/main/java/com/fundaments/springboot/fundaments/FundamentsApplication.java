02cc4d101865af1b97827a11c8849950e2de60c2
Santiago Salas <santiago.salas.contractor@bbva.com>
2023-05-15 12:22:23 -0500

    initial commit

---
+++ src/main/java/com/fundaments/springboot/fundaments/FundamentsApplication.java
@@ -0,0 +1,14 @@
+package com.fundaments.springboot.fundaments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentsApplication.class, args);
+		System.out.println("Start Fundaments of Spring Boot Project");
+	}
+
+}
---
+++ src/test/java/com/fundaments/springboot/fundaments/FundamentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundaments.springboot.fundaments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
