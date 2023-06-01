03c65bcc95087e2b1a5ec083be2f88e8bd41bb8e
Santiago Salas <santiago.salas.contractor@bbva.com>
2023-05-31 19:26:55 -0500

    Fin modulo configuraciones generales.

--- src/main/java/com/fundaments/springboot/fundaments/FundamentsApplication.java
+++ src/main/java/com/fundaments/springboot/fundaments/FundamentsApplication.java
@@ -4,6 +4,8 @@ import com.fundaments.springboot.fundaments.bean.MyBean;
 import com.fundaments.springboot.fundaments.bean.Operation.MyBeanWithDependence;
 import com.fundaments.springboot.fundaments.bean.Operation.MyOperation;
 import com.fundaments.springboot.fundaments.component.ComponentDependency;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -12,6 +14,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FundamentsApplication implements CommandLineRunner {
+
+	static Log LOGGER = LogFactory.getLog(FundamentsApplication.class);
 
 	private ComponentDependency componentDependency;
 	private MyBean myBean;
@@ -32,6 +36,12 @@ public class FundamentsApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentsApplication.class, args);
 		System.out.println(">>START Fundaments of Spring Boot Project<<<");
+		LOGGER.error("Log error de ejemplo en aplicacion");
 
+		try {
+			//Error
+		} catch (Exception exception) {
+			LOGGER.error("Log error de ejemplo en aplicacion");
+		}
 	}
 }
